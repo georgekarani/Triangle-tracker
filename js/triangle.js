decec5ff160af113025b2ffb903f36a2b40661a0
george <georgekarani77@gmail.com>
2019-06-03 10:33:35 +0300

    add updates to triangle.js and styles.css.

--- js/triangle.js
+++ js/triangle.js
@@ -1,32 +1,32 @@
 function triangle() {
 
-  //create an array to store the sides
+  //*****Note to self:create an array to store the sides****
   var sides = [];
 
-  //push all the sides into the array
+  //******Note to self:push all the sides into the array*****
   sides.push(document.getElementById("num1").value);
   sides.push(document.getElementById("num2").value);
   sides.push(document.getElementById("num3").value);
 
-  //assign the values of the array to variables a,b,c
+  //*****Note to self:assign the values of the array to variables a,b,c****
   var [a,b,c] = sides;
   var display = document.getElementById("display");
   var explanation = document.getElementById("explanation");
   if(a.length === 0 || b.length === 0 || c.length === 0 ) {
 
-    //check whether the user has typed something
+    //****Note to self:check whether the user has typed something****
     display.innerHTML = "<p>Please <span class='fill'>FILL</span> all the fields</p>"
   } else {
 
-    //convert the strings to numbers
+    //******Note to self:convert the strings to numbers*****
     a = parseFloat(a);
     b = parseFloat(b);
     c = parseFloat(c);
 
-    //track the triangle
+    //****always trace the triangle*****
     track(a,b,c);
   }
-
+   //*****Note to self:complete the function acc to lms*****
 }
 function track(a,b,c) {
   if(a + b > c && b + c > a && c + a > b) {
